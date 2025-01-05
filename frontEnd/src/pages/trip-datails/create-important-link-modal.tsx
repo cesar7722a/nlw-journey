2c7dbea7e87f11629e1d265305cfe7325a7e8f17
César Filipe António <cesarfilipeantonio2404@gmail.com>
2025-01-05 19:34:24 +0100

    feat:ligando o component create-importent-link-modal com back

--- frontEnd/src/pages/trip-datails/create-important-link-modal.tsx
+++ frontEnd/src/pages/trip-datails/create-important-link-modal.tsx
@@ -1,5 +1,8 @@
-import { Tag, X } from "lucide-react";
+import { Link2, Tag, X } from "lucide-react";
 import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
+import { FormEvent } from "react";
+import { api } from "../../lib/axios";
 
 interface CreateImportantLinkModalProps {
   ClosedCreateImportabtLinkModal: () => void;
@@ -8,37 +11,66 @@ interface CreateImportantLinkModalProps {
 export function CreateImportantLinkModal({
   ClosedCreateImportabtLinkModal,
 }: CreateImportantLinkModalProps) {
+  const { tripId } = useParams();
+
+  async function CreateLink(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+    const title = data.get(`titleLink`);
+    const url = data.get(`urlLink`);
+
+    console.log(title);
+    console.log(url);
+
+    await api.post(
+      `/trips/${tripId}/links
+    `,
+      {
+        title,
+        url,
+      }
+    );
+
+    // ClosedCreateImportabtLinkModal();
+    window.document.location.reload();
+  }
+
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
       <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
         <div className="space-y-2">
           <div className="flex items-center justify-between">
-            <h2 className="text-lg font-semibold">Cadastrar Atividade</h2>
+            <h2 className="text-lg font-semibold">Cadastrar Link</h2>
             <button type="button">
-              <X className="size-5 text-zinc-400" />
+              <X
+                className="size-5 text-zinc-400"
+                onClick={ClosedCreateImportabtLinkModal}
+              />
             </button>
           </div>
           <p className="text-sm text-zinc-400 text-left">
-            Todos convidados podem viasualizar as atividades
+            Todos convidados podem viasualizar os Links
           </p>
         </div>
 
-        <form className="space-y-3">
+        <form onSubmit={CreateLink} className="space-y-3">
           <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
             <Tag className="text-zinc-400 size-5" />
             <input
-              name="title"
-              placeholder="Qual a Atividade?"
-              className="bg-transparent text-lg placeholder-zinc-400 w-48 outline-none"
+              name="titleLink"
+              placeholder="Qual o link?"
+              className="bg-transparent text-lg placeholder-zinc-400 w-full  outline-none"
             />
           </div>
 
           <div className="flex items-center gap-2">
             <div className="h-14 flex-1 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+              <Link2 className="text-zinc-400 size-5" />
               <input
-                type="datetime-local"
-                name="occurs_at"
-                placeholder="Data e horário da  atividade"
+                type="text"
+                name="urlLink"
+                placeholder="A url do link que desejas cadastrar"
                 className="bg-transparent text-lg placeholder-zinc-400 flex-1 w-48 outline-none"
               />
             </div>
--- frontEnd/src/pages/trip-datails/important-links.tsx
+++ frontEnd/src/pages/trip-datails/important-links.tsx
@@ -1,50 +1,55 @@
 import { Link2, Plus } from "lucide-react";
 import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
 
 interface ImportantLinkProps {
   OpenCreateImportabtLinkModal: () => void;
 }
 
+interface Links {
+  id: string;
+  title: string;
+  url: string;
+}
+
 export function ImportantLink({
   OpenCreateImportabtLinkModal,
 }: ImportantLinkProps) {
+  const { tripId } = useParams();
+  const [links, setLinks] = useState<Links[]>([]);
+
+  useEffect(() => {
+    api
+      .get(`/trips/${tripId}/links`)
+      .then((response) => setLinks(response.data.links));
+  }, [tripId]);
+
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Links Importantes</h2>
 
-      <div className="space-y-5">
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">
-              Reserva AirBnB
-            </span>
-            <a
-              href="#"
-              className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
-            >
-              https://www.airbnb.com.br/rooms/18470001174y437848748y487438734t7843t48t484t843t478
-            </a>
+      {links.map((link) => {
+        return (
+          <div key={link.id} className="space-y-5">
+            <div className="flex items-center justify-between gap-4">
+              <div className="space-y-1.5">
+                <span className="block font-medium text-zinc-100">
+                  {link.title}
+                </span>
+                <a
+                  href={link.url}
+                  className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+                >
+                  {link.url}
+                </a>
+              </div>
+              <Link2 className="text-zinc-400 size-5 shrink-0" />
+            </div>
           </div>
-          <Link2 className="text-zinc-400 size-5 shrink-0" />
-        </div>
-      </div>
-
-      <div className="space-y-5">
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">
-              Reserva AirBnB
-            </span>
-            <a
-              href="#"
-              className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
-            >
-              https://www.airbnb.com.br/rooms/18470001174y437848748y487438734t7843t48t484t843t478
-            </a>
-          </div>
-          <Link2 className="text-zinc-400 size-5 shrink-0" />
-        </div>
-      </div>
+        );
+      })}
 
       <Button
         variant="secondary"
